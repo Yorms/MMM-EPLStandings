5c101f80aebac065805b67a5d7af86699adca315
Yorms <eliwood232@live.com>
2024-11-25 22:45:51 -0500

    Add to Config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+modules: [
+    {
+        module: "MMM-EPLStandings",
+        position: "bottom_right", // You can choose any position
+        config: {
+            apiKey: "YOUR_API_KEY", // Replace with your Football-Data.org API key
+            maxTeams: 14           // Number of teams to display
+        }
+    }
+]
